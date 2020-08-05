73ace70e2a50c73320a4be9f63a435eed74be82f
qw123gz <364768428@qq.com>
2020-08-05 18:32:28 +0800

    a

--- pages/user/cash/cash.vue
+++ pages/user/cash/cash.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="index">
 		<view class="main" v-if="dataList.length>0">
-			<view class="item">
+			<view class="item" v-for="(item,index) in 5" :key="index">
 				<view class="item-label">
 					<view class="label-name">
 						套餐分成
@@ -9,6 +9,9 @@
 					<view class="label-count">
 						+200
 					</view>
+					<!-- <view class="label-count active">
+						+200
+					</view> -->
 				</view>
 				<view class="item-date">
 					07-02 09:26:48
@@ -24,7 +27,7 @@
 	import NoRecord from '@/components/NoRecord.vue'
 	import noMore from "@/components/noMore.vue"
 	export default {
-		components:{
+		components: {
 			NoRecord,
 			noMore
 		},
@@ -37,11 +40,47 @@
 				uid: ''
 			}
 		},
+		onReachBottom() {
+			console.log(this.isMore)
+			if (this.isMore) {
+				this.getShopList()
+			}
+		},
 		onLoad() {
-
+			this.uid = uni.getStorageSync('uid')
+			this.getShopList()
 		},
 		methods: {
-
+			getShopList() {
+				let data = {
+					cmd: 'getuserwithdraw',
+					clientid: this.$clientid.index,
+					sign: this.$clientid.sign,
+					page: this.page,
+					pagesize: this.pagesize,
+					uid: this.uid
+				}
+				this.$post('', data).then((res) => {
+						console.log(res)
+						if (res.status == 0) {
+							let data = res.response
+							this.dataList = this.dataList.concat(data)
+							let isMore = data.length < this.pagesize
+							if (isMore) {
+								this.isMore = false
+							} else {
+								this.isMore = true
+								this.page = this.page + 1
+							}
+						} else {
+							this.isMore = false
+						}
+					}),
+					((err) => {
+						console.log(err)
+						this.isMore = false
+					})
+			},
 		}
 	}
 </script>
@@ -50,49 +89,55 @@
 	.index {
 		width: 100%;
 		overflow: hidden;
-		.main{
+
+		.main {
 			width: 100%;
 			padding: 0 30upx;
 			box-sizing: border-box;
-			.item{
+
+			.item {
 				width: 100%;
-				padding:30upx 0;
+				padding: 30upx 0;
 				box-sizing: border-box;
 				border-bottom: 1px solid #E4E4E4;
-				.item-label{
+
+				.item-label {
 					width: 100%;
 					height: 50upx;
 					display: flex;
-					.label-name{
+
+					.label-name {
 						flex: 1;
-						font-size:30upx;
-						font-family:PingFang SC;
-						font-weight:500;
-						color:rgba(49,48,48,1);
+						font-size: 30upx;
+						font-family: PingFang SC;
+						font-weight: 500;
+						color: rgba(49, 48, 48, 1);
 					}
-					.active{
-						color:rgba(49,48,48,1) !important;
+
+					.active {
+						color: rgba(49, 48, 48, 1) !important;
 					}
-					.label-count{
+
+					.label-count {
 						flex: 1;
 						text-align: right;
-						font-size:36upx;
-						font-family:DIN 1451 Mittelschrift;
-						font-weight:400;
-						color:rgba(228,195,125,1);
+						font-size: 36upx;
+						font-family: DIN 1451 Mittelschrift;
+						font-weight: 400;
+						color: rgba(228, 195, 125, 1);
 					}
 				}
-				.item-date{
+
+				.item-date {
 					width: 100%;
 					height: 40upx;
 					line-height: 40upx;
-					font-size:24upx;
-					font-family:PingFang SC;
-					font-weight:500;
-					color:rgba(145,146,151,1);
+					font-size: 24upx;
+					font-family: PingFang SC;
+					font-weight: 500;
+					color: rgba(145, 146, 151, 1);
 				}
 			}
 		}
 	}
-
 </style>
